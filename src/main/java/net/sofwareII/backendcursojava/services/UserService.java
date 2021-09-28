b3f4f4df59e355e00dca510e661b628b26c6eb59
sebastian Rojas <sebastian.rojas39@correo.tdea.edu.co>
2021-09-27 21:46:54 -0500

    conectar a la base de datos

--- src/main/java/net/sofwareII/backendcursojava/services/UserService.java
+++ src/main/java/net/sofwareII/backendcursojava/services/UserService.java
@@ -1,16 +1,35 @@
 package net.sofwareII.backendcursojava.services;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.sofwareII.backendcursojava.UserRepository;
+import net.sofwareII.backendcursojava.entities.UserEntity;
 import net.sofwareII.backendcursojava.shared.dto.UserDTO;
 
 @Service
 public class UserService implements UserServiceInterface {
 
+    
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDTO createUser(UserDTO userDto) {
-        // TODO logica de la creacion del usuario
-        return null;
+        
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDto, userEntity);
+
+        userEntity.setEncryptedPassword("testpassword");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetail = userRepository.save(userEntity);
+
+        UserDTO userToReturn = new UserDTO();
+        BeanUtils.copyProperties(storedUserDetail, userToReturn);
+        return userToReturn;
     }
 
 }
