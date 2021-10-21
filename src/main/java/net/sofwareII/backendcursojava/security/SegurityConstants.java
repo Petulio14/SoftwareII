445d216eaa2a3511ac5e1affaf1cb5d7919645bf
sebastian Rojas <sebastian.rojas39@correo.tdea.edu.co>
2021-10-20 21:42:29 -0500

    Agregamos Autenticacion Spring.security

---
+++ src/main/java/net/sofwareII/backendcursojava/security/SegurityConstants.java
@@ -0,0 +1,10 @@
+package net.sofwareII.backendcursojava.security;
+
+public class SegurityConstants {
+    
+    public static final long EXPIRATION_DATE = 864000000;
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String TOKEN_STRING = "Authorization";
+    public static final String SING_UP_URL  = "/users";
+    public static final String TOKEN_SECRET = "8Aj7X2kg2ZDhIci2HN3kQWk1M0HykVFd";
+}
